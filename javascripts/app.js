6f64693c68776dc2ed8ddfd4a5059be3a94053fb
justin leggett <justin@codingrookie.com>
2016-11-14 21:32:26 -0600

    completed day 1

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,5 +1,42 @@
 "use strict";
 
-console.log("inside app.js")
+var app = angular.module("TodoApp", []);
+
+app.controller("NavCtrl", ($scope) => {
+    $scope.navItems = [{name: "Logout"}, {name:"All Items"}, {name:"New Item"}];
+});
 
 
+app.controller("TodoCtrl", ($scope) => {
+    $scope.welcome = "hello";
+    $scope.showListView = true;
+    $scope.items = [
+        {
+            id: 0,
+            task: "mow the lawn",
+            isCompleted: true,
+            assignedTo: "Zoe"
+        },
+        {
+            id: 1,
+            task: "mow the grass",
+            isCompleted: false,
+            assignedTo: "Justin"
+        },
+        {
+            id: 2,
+            task: "clean the house",
+            isCompleted: false,
+            assignedTo: "Katie"
+        }
+    ];
+
+    $scope.allItems = () => {
+        $scope.showListView = true;
+    };
+
+    $scope.newItem = () => {
+        $scope.showListView = false;
+    };
+
+});
