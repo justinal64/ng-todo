460425570814d592d7a1afa781fac6bd40687447
justin leggett <justin@codingrookie.com>
2016-11-22 21:01:38 -0600

    completed assignment

--- javascripts/controllers/NavCtrl.js
+++ javascripts/controllers/NavCtrl.js
@@ -1,8 +1,7 @@
 "use strict";
 
 app.controller("NavCtrl", ($scope) => {
-    $scope.navItems = [{name:"Auth", url: "#/auth"},
-    {name: "Logout", url: "#/logout"},
+    $scope.navItems = [{name: "Logout", url: "#/logout"},
     {name:"All Items", url: "#/items/list"},
     {name:"New Item", url: "#/items/new"}];
 });
