88be9db4ee3093abee539ff7871dbc03c5f56acd
justin leggett <justin@codingrookie.com>
2016-11-22 20:53:24 -0600

    completed the application

--- javascripts/AppConfig.js
+++ javascripts/AppConfig.js
@@ -1,7 +1,31 @@
 "use strict";
 
-app.run((FIREBASE_CONFIG) => {
+let isAuth = (AuthFactory) => new Promise((resolve, reject) => {
+    if(AuthFactory.isAuthenticated()) {
+        resolve();
+    } else {
+        reject();
+    }
+});
+
+app.run(($rootScope, $location, FIREBASE_CONFIG, AuthFactory) => {
      firebase.initializeApp(FIREBASE_CONFIG);
+
+     $rootScope.$on('$routeChangeStart', function(event, currRoute, prevRoute) {
+
+        let logged = AuthFactory.isAuthenticated();
+        let appTo;
+
+        if(currRoute.originalPath) {
+            appTo = currRoute.originalPath.indexOf('/auth') !== -1;
+        }
+
+        if(!appTo && !logged) {
+            event.preventDefault();
+            $location.path('/auth');
+        }
+
+     });
 });
 
 app.config(function($routeProvider) {
@@ -12,20 +36,29 @@ app.config(function($routeProvider) {
         })
         .when('/items/list', {
             templateUrl: 'partials/item-list.html',
-            controller: 'ItemListCtrl'
+            controller: 'ItemListCtrl',
+            resolve: {isAuth}
         })
         .when('/items/new', {
             templateUrl: 'partials/item-new.html',
-            controller: 'ItemNewCtrl'
+            controller: 'ItemNewCtrl',
+            resolve: {isAuth}
         })
         .when('/items/view/:id', { // colon means the value will change
             templateUrl: 'partials/item-view.html',
-            controller: 'ItemViewCtrl'
+            controller: 'ItemViewCtrl',
+            resolve: {isAuth}
         })
         .when('/items/edit/:id', {
             templateUrl: 'partials/item-new.html',
-            controller: 'ItemEditCtrl'
+            controller: 'ItemEditCtrl',
+            resolve: {isAuth}
         })
-        .otherwise('/items/list'); // This could be a 404 error...
+        .when('/logout', {
+            templateUrl: 'partials/auth.html',
+            controller: 'AuthCtrl',
+            resolve: {isAuth}
+        })
+        .otherwise('/auth'); // This could be a 404 error...
 
 });
